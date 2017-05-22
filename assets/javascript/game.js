573dbc8b83249c9921bddf18d547c450df1f53e6
Ryan Hoyda <Ryanhoyda@gmail.com>
2017-05-21 17:30:19 -0700

    adding character click event listeners

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,21 @@
 /**
  * Created by ryanhoyda on 5/15/17.
  */
+
+
+//Add click event listeners for our characters:
+$("#pick_luke").click(function(){
+    alert('Picked Luke SkyWalker');
+});
+
+$("#pick_Obi").click(function(){
+    alert('Picked Obi-wan-kenobi');
+});
+
+$("#pick_Maul").click(function(){
+    alert('Picked Darth Maul');
+});
+
+$("#pick_Sidious").click(function(){
+    alert('Picked Darth Sidious');
+});
