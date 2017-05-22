a3d1d4b433b26f845c7cf036e9c84f2d818587fd
Ryan Hoyda <Ryanhoyda@gmail.com>
2017-05-21 17:56:39 -0700

    swap out image - reference on line 54 with chosen character.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,18 +7,52 @@
 $(document).ready(function() {
 
     $("#pick_Luke").click(function () {
-        alert('Picked Luke SkyWalker');
+        //alert('Picked Luke SkyWalker');
+        var html = $(this).html();
+
+        $("#your_character")
+            .empty() //Blow out the existing html in that div
+            .append(html); //Add the html in this div to the your character div
+
+        //We will update this later
+        $(this).empty();
     });
 
     $("#pick_Obi").click(function () {
-        alert('Picked Obi-wan-kenobi');
+        //alert('Picked Obi-wan-kenobi');
+        var html = $(this).html();
+
+        $("#your_character")
+            .empty() //Blow out the existing html in that div
+            .append(html); //Add the html in this div to the your character div
+
+        //We will update this later
+        $(this).empty();
     });
 
     $("#pick_Maul").click(function () {
-        alert('Picked Darth Maul');
+        //alert('Picked Darth Maul');
+        var html = $(this).html();
+
+        $("#your_character")
+            .empty() //Blow out the existing html in that div
+            .append(html); //Add the html in this div to the your character div
+
+        //We will update this later
+        $(this).empty();
     });
 
     $("#pick_Sidious").click(function () {
-        alert('Picked Darth Sidious');
+        //alert('Picked Darth Sidious');
+        var html = $(this).html();
+
+        $("#your_character")
+            .empty() //Blow out the existing html in that div
+            .append(html); //Add the html in this div to the your character div
+
+        //We will update this later
+        $(this).empty();
     });
 });
+
+
